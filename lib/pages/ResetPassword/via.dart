4f38623116b900df31a9bd3928b4dc6b5baa08d0
Sara <sn.khiter@esi-sba.dz>
2024-05-02 13:29:31 +0000

    ~

--- lib/pages/ResetPassword/via.dart
+++ lib/pages/ResetPassword/via.dart
@@ -1,6 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+class CustomButtonTile extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  final String subtitle;
+  final VoidCallback onPressed;
+
+  const CustomButtonTile({
+    Key? key,
+    required this.iconData,
+    required this.title,
+    required this.subtitle,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+         ),
+         backgroundColor: Color.fromARGB(255,255,255,255),
+          elevation: 2,
+      ),
+        child: Row(
+          children: [
+            Icon(
+              iconData,
+              color: Color.fromARGB(255,243, 159, 90),
+             size: 40.0,
+              ),
+            SizedBox(width: 30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                    fontSize: 16,
+                     fontWeight: FontWeight.bold,
+                    color: Colors.black),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+  }
+}
+
 class via extends StatefulWidget {
   const via({Key? key}) : super(key: key);
 
@@ -9,21 +69,17 @@ class via extends StatefulWidget {
 }
 
 class _viaState extends State<via> {
-  Color MainColor = Color.fromARGB(255,243, 159, 90); 
-  
+Color MainColor = Color.fromARGB(255,243, 159, 90); //icon,text,border
+
   @override
   Widget build(BuildContext context) {
-    String phoneNumber = '+1234567890';
-    final Size screenSize = MediaQuery.of(context).size;
-  final double buttonWidth = screenSize.width * 0.8372; // 70% of screen width
-    final double buttonHeight = screenSize.height * 0.1126;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      body: Stack(
+        resizeToAvoidBottomInset : false,
+        backgroundColor: Colors.white,
+           body:  Stack(
         fit: StackFit.expand,
-         children: [
-          Positioned(
+        children: [
+              Positioned(
       top: MediaQuery.of(context).size.height * 0.0989,
       left: MediaQuery.of(context).size.width * 0.0605,
       child: Container(
@@ -40,13 +96,13 @@ class _viaState extends State<via> {
         size: 40.0,
       ),
     ),    
-  ),  
-          Container(
+  ), 
+  Container(
               margin: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height * 0.1641,
                   left: MediaQuery.of(context).size.width * 0.0605),
               child: Text(
-                'Forgot Password?',
+                'Forgot password ?',
                 style: GoogleFonts.poppins(
                     textStyle: TextStyle(
                         fontSize: 27,
@@ -54,16 +110,12 @@ class _viaState extends State<via> {
                         color: MainColor),
                     height: 1.0),
               )),
-              SizedBox(
-              height: 5,
-            ),
-               Container(
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.2177,
-                  left: MediaQuery.of(context).size.width * 0.0605,
-                  right: MediaQuery.of(context).size.width * 0.0605),
+  Container (
+    margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.23,
+                  left: MediaQuery.of(context).size.width * 0.0605),
               child: Text(
-                'Select which contact details should we use to reset your password',
+                'Select which contact details should \n we use to reset your password',
                 style: GoogleFonts.poppins(
                     textStyle: TextStyle(
                         fontSize: 12,
@@ -71,68 +123,49 @@ class _viaState extends State<via> {
                         color: MainColor),
                     height: 1.0),
               )),
-              /* SizedBox(
-                  height:30.0,
-                 ) ,  */
-               Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () { },
-                child: ListTile(
-              leading: Icon(Icons.sms,
-              color: MainColor,
-                     size: 40.0,),
-              title: Text('Via sms:',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.black,)),
-              subtitle: Text(
-                '.... .... 4235',
-                   style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,) 
-                  ),
-            ),
-              ),
-              SizedBox(
-                  height:10.0,
-                 ) ,
-              ElevatedButton(
-                onPressed: () { },
-                child: ListTile(
-              leading: Icon(Icons.email,
-              color: MainColor,
-                     size: 30.0,),
-              title: Text('Via email:',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,)),
-              subtitle: Text(
-                   '.... @gmail.com',
-                   style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,) 
-                  ),
-            ),
-              ),
-              SizedBox(
-                  height:60.0,
-                 ) ,
-              ]
-              ),
-              Container(
-                margin: EdgeInsets.only(
+    Container(  
+       margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.3164,
+                  left: MediaQuery.of(context).size.width * 0.0814,
+                  bottom : MediaQuery.of(context).size.height *  0.5710,
+                  right: MediaQuery.of(context).size.width * 0.0814),
+              height: MediaQuery.of(context).size.height * 0.1127,
+              width: MediaQuery.of(context).size.width * 0.8372,
+              child: CustomButtonTile(
+  iconData: Icons.sms,
+  title: 'Via sms',
+  subtitle: '.... .... 4235',
+  onPressed: () {
+    // Handle button tap here
+  },
+),
+    ),
+        Container(  
+       margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.4422,
+                  left: MediaQuery.of(context).size.width * 0.0814,
+                  bottom : MediaQuery.of(context).size.height *  0.4445,
+                  right: MediaQuery.of(context).size.width * 0.0814),
+              height: MediaQuery.of(context).size.height * 0.1127,
+              width: MediaQuery.of(context).size.width * 0.8372,
+              child: CustomButtonTile(
+  iconData: Icons.email,
+  title: 'Via email',
+  subtitle: '.... @gmail.com',
+  onPressed: () {
+    // Handle button tap here
+  },
+),
+    ),
+     Container(
+      margin: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height * 0.6072,
-                  left: MediaQuery.of(context).size.width * 0.1,
-                  right: MediaQuery.of(context).size.width * 0.1,
-                  bottom: MediaQuery.of(context).size.height * 0.335,),
+                  left: MediaQuery.of(context).size.width * 0.0814,
+                  bottom : MediaQuery.of(context).size.height *  0.336,
+                  right: MediaQuery.of(context).size.width * 0.0814),
               child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                  },
                   style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10)),
@@ -150,8 +183,10 @@ class _viaState extends State<via> {
                     ),
                   )),
             )
-         ]
-      )
-      );
+       
+        ],         
+  ),
+);
+          
   }
 }
